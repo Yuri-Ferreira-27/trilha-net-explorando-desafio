74a7ae57395afab42a254624d61276f402710e65
Yuri Ferreira <yuri.ferreira@soulasalle.com.br>
2023-01-10 21:41:28 -0300

    Desafio 2 Concluído do Bootcamp da DIO/Pottential .NET Developer

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -16,15 +17,17 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // IMPLEMENTAÇÃO REALIZADA
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // IMPLEMENTAÇÃO REALIZADA
+                
+                throw new Exception("Exceção Ocorrida. O número de hóspedes informado é maior do que a capacidade total");
             }
         }
 
@@ -36,22 +39,24 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // IMPLEMENTAÇÃO REALIZADA
+            int qtdHospedes = Hospedes.Count();
+            return qtdHospedes;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // IMPLEMENTAÇÃO REALIZADA
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // IMPLEMENTAÇÃO REALIZADA
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valor - (valor/10);
+                return valor;
             }
 
             return valor;
--- Program.cs
+++ Program.cs
@@ -6,17 +6,28 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa();
+Pessoa p2 = new Pessoa();
+Pessoa p3 = new Pessoa();
+
+Console.WriteLine($"Informe o nome do 1º Hóspede: ");
+p1.Nome = Console.ReadLine();
+
+Console.WriteLine($"Informe o nome do 2º Hóspede: ");
+p2.Nome = Console.ReadLine();
+
+Console.WriteLine($"Informe o nome do 3º Hóspede: ");
+p3.Nome = Console.ReadLine();
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
 
 // Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
